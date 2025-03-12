100d07f467b2c06dd6e31d0775b5cc48e3410f8c
ersekcsongor <125259385+ersekcsongor@users.noreply.github.com>
2025-03-12 23:12:00 +0200

    corrected countries insertion

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove properties not in the DTO
+      forbidNonWhitelisted: true, // Throw error if non-whitelisted properties are found
+      transform: true, // Transform payloads to DTO instances
+    })
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
